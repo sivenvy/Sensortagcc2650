3cd781cc0d2d7e95e515ce1c5bd924fd36bf688e
sivenvy <sivenvy999@gmail.com>
2017-05-05 17:31:11 +0800

    Update and rename LICENSE.md to stag2.js

---
+++ stag2.js
@@ -0,0 +1,35 @@
+$ sudo node sensortag2dweet.js
+discovered: {"id":"bbb448ba4287","type":"cc2650"}
+http://dweet.io/follow/cc2650-bbb448ba4287
+connectAndSetUp
+enableHumidity
+setHumidityPeriod
+notifyHumidity
+        temperature = 19.5 °C
+        humidity = 47.9 %
+        temperature = 19.5 °C
+        humidity = 47.9 %
+        temperature = 19.5 °C
+        humidity = 48 %
+cc2650-bbb448ba4287
+{ temperature: 19.5, humidity: 47.9 }
+Fri Dec 18 2015 13:17:55 GMT+0000 (UTC)
+        temperature = 19.5 °C
+        humidity = 47.9 %
+        temperature = 19.5 °C
+        humidity = 47.9 %
+        temperature = 19.5 °C
+        humidity = 47.9 %
+        temperature = 19.5 °C
+        humidity = 47.9 %
+        temperature = 19.5 °C
+        humidity = 47.9 %
+        temperature = 19.5 °C
+        humidity = 47.9 %
+cc2650-bbb448ba4287
+{ temperature: 19.5, humidity: 47.9 }
+Fri Dec 18 2015 13:18:01 GMT+0000 (UTC)
+        temperature = 19.5 °C
+        humidity = 47.9 %
+ :
+ :
